b7c353b02dae19a5833533483a93d73f4cb224d3
Makena <roselynemakena@gmail.com>
2018-09-18 18:08:27 +0300

    Correct checkstyle

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,12 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+  /**
+   *
+   * @param someone
+   * @return greeting String
+   */
+  public final String greet(String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
