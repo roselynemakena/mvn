46cf1d9b05893f93ca194d915f8e0a0cb2b909a5
Makena <roselynemakena@gmail.com>
2018-09-20 14:42:34 +0300

    Edit home

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -17,7 +17,7 @@ public class Greeter {
    * @param someone
    * @return greeting String
    */
-  public final String greet(String someone) {
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
--- webapp/src/main/webapp/index.jsp
+++ webapp/src/main/webapp/index.jsp
@@ -1,1 +1,2 @@
 Hello, people and the other animals that live in this world!
+There was once a farmer and his farm, he lived by the lake of Galilee!
